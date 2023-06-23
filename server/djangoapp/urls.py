6aa8f82c66214c57cbcb41eafe876181c1651381
geraflores <vasago2014@hotmail.com>
2023-06-23 10:09:02 -0400

    semana 2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # route is a string contains a URL pattern
+    # path(route='', view=views.CourseListView.as_view(), name='popular_index'),
     # view refers to the view function
     # name the URL
 
@@ -14,10 +15,13 @@ urlpatterns = [
     # path for contact us view
     path(route='contact', view=views.contact, name='contact'),
     # path for registration
+    path('registration/', views.registration_request, name='registration'),
 
     # path for login
+    path('login/', views.login_request, name='login'),
 
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
@@ -26,3 +30,4 @@ urlpatterns = [
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
 # from .restapis import related methods
+
+from django.urls import reverse
+from django.views import generic, View
+
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -35,13 +39,65 @@ def contact(request):
 # def login_request(request):
 # ...
 
+def login_request(request):
+    context = {}
+    # Handles POST request
+    if request.method == "POST":
+        # Get username and password from request.POST dictionary
+        username = request.POST['username']
+        password = request.POST['psw']
+        # Try to check if provide credential can be authenticated
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None:
+            # If user is valid, call login method to login current user
+            login(request, user)
+            return render(request, 'djangoapp/index.html', context)
+        else:
+            # If not, return to login page 
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/index.html', context)
+    else:
+        return render(request, 'djangoapp/index.html', context)
+
+
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
+def logout_request(request):
+    logout(request)
+    return render(request, 'djangoapp/index.html')
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
+def registration_request(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    elif request.method == 'POST':
+        # Check if user exists
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.debug("{} is new user".format(username))
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return render(request, 'djangoapp/index.html', context)
+
+        else:
+            context['message'] = "User already exists."
+            return render(request, 'djangoapp/registration.html', context)
+
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
@@ -57,4 +113,12 @@ def get_dealerships(request):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
+# Add a class-based course list view
+#class CourseListView(generic.DetailView):
+ #   template_name = 'django/index.html'
+    
+
+
+
+
 
