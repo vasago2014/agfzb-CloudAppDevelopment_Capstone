b48aefbb1a98a1392022158ee41bac4f634eb2f5
gerardo flores <vasago2014@hotmail.com>
2023-06-28 14:02:22 -0400

    semana 3

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1
+
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'year')
+    list_filter = ['year']
+    search_fields = ['name', 'type']
+
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
+
+    
 # Register your models here.
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
+
+
 
 # CarModelInline class
 
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +26,27 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+    id = models.IntegerField(default=1,primary_key=True)
+    name = models.CharField(max_length=1000)
+    dealer_id = models.CharField(max_length=1000)
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON')
+    ]
+    type = models.CharField(max_length=1000) 
+    year = models.DateField(null=True)
+    cars_make = models.ForeignKey(CarMake, null=False, on_delete=models.CASCADE)
+   
+
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
